2d61a022524d0f1dd61b130efb595bc067d876e8
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-24 12:31:20 +0200

    feat: initial student management system setup
    - Added student struct definition
    - Created student array structure
    - Defined constants and subjects

--- C-Essentials/projects/studentManagement.c
+++ C-Essentials/projects/studentManagement.c
@@ -1,4 +1,35 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NAME_LENGTH 20
+#define GRADES_COUNT 5
+
+const char SUBJECTS[][10] = { "IoT", "C", "MS-Office", "English", "Math" };
+
+struct Student {
+  int index;
+  unsigned int id;
+  char name[NAME_LENGTH];
+  unsigned short grades[GRADES_COUNT];
+  float averageGrades;
+};
+
+struct StudentArray {
+  struct Student* students; // Pointer to array of students
+  unsigned int count; // Current number of students
+  unsigned int capacity; // Total allocated capacity
+};
+
+const struct Student NotFound = {
+  .index = -1,
+  .id = 0,
+  .name = "",
+  .grades = {0, 0, 0, 0, 0},
+  .averageGrades = 0.0
+};
+
 
 int main() {
   printf("hello world\n");

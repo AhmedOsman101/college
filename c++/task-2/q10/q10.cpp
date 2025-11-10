c46707ace2c192814c5c169e16bcc3e90db59306
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-11-10 21:35:00 +0200

    Updated Files 2025-11-10@09:35PM

--- c++/task-2/q10/q10.cpp
+++ c++/task-2/q10/q10.cpp
@@ -4,26 +4,27 @@
 using namespace std;
 
 
-unsigned long long factorial(unsigned long long n) {
+int factorial(int n) {
   if (n <= 2) return n;
 
-  unsigned long long result = 1;
+  int result = 1;
 
-  for (unsigned long long i = 2; i <= n; i++) result *= i;
+  for (int i = 2; i <= n; i++) result *= i;
 
   return result;
 }
 
 int main() {
-  unsigned long long x = 3, n;
+  int x = 3, n;
   double y = 0;
   cout << "Enter upper bound: ";
   cin >> n;
-  printf("%llu\n", factorial(n));
 
   for (int i = 1; i < n; i++) {
     y += pow((5 + x) / (2 * x), i) * (1.0 / factorial(i));
   }
+
+  cout << "y = " << y << endl;
 
   return 0;
 }
